f6cfd29a33c0721d84124182b8b107e9cd2ab9cd
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-06-20 10:46:54 +0200

    cambio simbols

--- js/coinselector.js
+++ js/coinselector.js
@@ -2,6 +2,7 @@
 
 const spanProfPrice = document.getElementById("prof-price");
 const spanpremPrice = document.getElementById("premium-price");
+const spanbasicPrice = document.getElementById("basic-price");
 
 let select = document.getElementById('mySelect');
 select.addEventListener('change', ()=>{
@@ -11,18 +12,20 @@ select.addEventListener('change', ()=>{
     .then(response => response.json())
     .then(data => {
       if (selectedOption === "option2"){
-       
+        spanbasicPrice.textContent = "$" + 0;
         spanProfPrice.textContent = "$" + Math.round(25 * data.eur.usd);
         spanpremPrice.textContent = "$" + Math.round(60 * data.eur.usd);
         console.log(data.eur.usd);
 
       }else if(selectedOption==="option3"){
+        spanbasicPrice.textContent = "£" + 0;
         spanpremPrice.textContent = "£" + Math.round(60 * data.eur.gbp);
         spanProfPrice.textContent = "£" + Math.round(25 * data.eur.gbp);
         console.log(data.eur.gbp)
       }else{
-        spanProfPrice.textContent ="$" + 25;
-        spanpremPrice.textContent = "$" + 60;
+        spanbasicPrice.textContent = "€" + 0;
+        spanProfPrice.textContent ="€" + 25;
+        spanpremPrice.textContent = "€" + 60;
       }
     })
     .catch(error => {
